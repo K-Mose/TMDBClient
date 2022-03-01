ad94d2eb095cde59b514eff7ce9017a74fcf0d6b
user <km_413@naver.com>
2022-03-01 17:08:03 +0900

    Create Data layer 2 - retrofit service interface

---
+++ app/src/main/java/com/kmose/tmdbclient/data/api/TMDBService.kt
@@ -0,0 +1,26 @@
+package com.kmose.tmdbclient.data.api
+
+import com.kmose.tmdbclient.data.ArtistList
+import com.kmose.tmdbclient.data.MovieList
+import com.kmose.tmdbclient.data.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+    // MovieList class 형태로 JSON 데이터가 들어옴
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey:String
+    ): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(
+        @Query("api_key") apiKey:String
+    ): Response<TvShowList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtist(
+        @Query("api_key") apiKey:String
+    ): Response<ArtistList>
+}
